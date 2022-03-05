25bd5edd1060e5777c3f0a37c1f6194ab703ce48
Renato Fagalde <renato@likwi.com.br>
2022-03-05 20:26:21 -0300

    Packages and H2 up running

--- src/test/java/br/com/likwi/sso/SSOApplicationTests.java
+++ src/test/java/br/com/likwi/sso/SSOApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SsoApplicationTests {
+class SSOApplicationTests {
 
     @Test
     void contextLoads() {
